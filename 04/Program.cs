92de313834c22adcb076930bbd48dd3976fdef9a
AnkaBigudi <ankabigudi@ya.ru>
2023-02-03 15:13:16 +0300

    Создание метода viod, FillArray, PrintArray c использованием генерирования случайных чисел

---
+++ 04/Program.cs
@@ -0,0 +1,22 @@
+﻿//Создание метода viod, FillArray, PrintArray c использованием генерирования случайных чисел
+
+void FillArray(int[] collection){
+  int length = collection.Length;
+  int index = 0;
+  while(index < length){
+    collection[index] = new Random().Next (1, 10);
+    index++;
+  }
+
+}
+void PrintArray(int[] col){
+  int count = col.Length;
+  int position = 0;
+  while(position < count){
+    Console.WriteLine(col[position]);
+    position++;
+  }
+}
+int[] array = new int[10]; //в массиве 10 элементов
+FillArray(array); //заполнил массив
+PrintArray(array); // распечатал массив
